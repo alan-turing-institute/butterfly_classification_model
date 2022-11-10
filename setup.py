407537106b3ea6c309a7507867b9e333f464be43
Ed Chalstrey <edwardchalstrey@gmail.com>
2022-11-10 14:31:17 +0000

    cchange repo name and bump version

--- setup.py
+++ setup.py
@@ -10,9 +10,9 @@ with open("requirements.txt") as f:
 
 setup(
     name="butterfly_classification_model",
-    version="0.0.1",
+    version="0.0.2",
     description="scivision plugin, using EfficientNetB3 model",
-    url="https://github.com/nbarlowATI/butterfly_classification_model",
+    url="https://github.com/alan-turing-institute/butterfly_classification_model",
     packages=find_packages(),
     install_requires=requirements,
     python_requires=">=3.7",
